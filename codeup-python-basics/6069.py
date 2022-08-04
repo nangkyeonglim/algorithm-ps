41c84e793469ce3bb68f53e196553ab55d141715
nangkyeonglim <nangkyeonglim@gmail.com>
2022-08-04 23:05:19 +0900

    codeup 100제

---
+++ codeup-python-basics/6069.py
@@ -0,0 +1,12 @@
+data = input()
+
+if data == "A":
+    print("best!!!")
+elif data == "B":
+    print("good!!")
+elif data == "C":
+    print("run!")
+elif data == "D":
+    print("slowly~")
+else:
+    print("what?")
