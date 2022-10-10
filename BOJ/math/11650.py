b9dc0539a83f271c6b25fc45c119007e3e3cb0f6
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-11 00:20:28 +0900

    BOJ math

--- BOJ/math/11650.py
+++ BOJ/math/11650.py
@@ -1,6 +1,17 @@
+"""import sys
+
+n = int(sys.stdin.readline())
+coordinates = []
+
+for i in range(n):
+    coordinates.append(list(map(int, sys.stdin.readline().split())))
+
+coordinates.sort(key = lambda x: (x[0], x[1]))
+
+for i in range(n):
+    print(coordinates[i][0], coordinates[i][1])"""
+
 import sys
-
-_, *coordinates = map(lambda s: s.strip(), sys.stdin.readlines())
-
-for i in sorted(coordinates):
-    print(i)
+_, *dot = sys.stdin.readlines()
+dot.sort(key=lambda x: (int(x.split()[0]), int(x.split()[1])))
+print(''.join(dot))
