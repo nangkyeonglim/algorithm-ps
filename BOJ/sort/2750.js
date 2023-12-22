4a117552c282a53bddf9cda5af2f13e351795c73
nangkyeonglim <nangkyeonglim@gmail.com>
2023-12-22 17:22:06 +0900

    feat(2705): BOJ 수 정렬하기

---
+++ BOJ/sort/2750.js
@@ -0,0 +1,9 @@
+const input = require('fs')
+  .readFileSync('input.txt')
+  .toString()
+  .trim()
+  .split('\n');
+
+const numbers = input.slice(1).map(Number);
+
+console.log(numbers.sort((a, b) => a - b).join('\n'));
